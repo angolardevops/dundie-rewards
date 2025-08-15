3abe4e1196e6c0a795fb9868e772c794b53f5edb
Walter Angolar <angolar.devops@gmail.com>
2025-08-15 17:57:09 +0100

    Set up project initial organization and metadata
    - Add setup.py to turn installable
    - Add __main__ for executable
    - Add __init__.py for package
    
    closes #8

---
+++ dundie/__main__.py
@@ -0,0 +1,1 @@
+print("Executing main entry point for dundie package... no informação")
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dundie',
+    version='0.1.0',
+    author='Walter Angolar',
+    description='A Python package for managing and analyzing Dundie Awards data.',
+    packages=find_packages(),
+)
